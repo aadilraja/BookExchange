db3202933b3944718c3258e859eb327453147739
aadilraja <aadilraja208@gmail.com>
2025-07-18 21:59:09 +0530

    restarting the project

--- api/src/main/java/application/backend/Controller/AuthenticationController.java
+++
@@ -1,7 +0,0 @@
-package application.backend.Controller;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class AuthenticationController {
-}
--- api/src/main/java/application/backend/persistence/Repo/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package application.backend.persistence.Repo;
-
-import application.backend.persistence.Model.UserInfo;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepo extends CrudRepository<UserInfo,String> {
-}
