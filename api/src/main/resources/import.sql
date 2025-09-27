9cce71526627dfc4ef94a25c1adcb13b6d1956b0
aadilraja <aadilraja208@gmail.com>
2025-09-27 18:43:24 +0530

    updated import.sql

--- api/src/main/resources/import.sql
+++ api/src/main/resources/import.sql
@@ -1,4 +1,3 @@
-INSERT INTO role (id, name, created_at, updated_at) VALUES
-                                                        (1, 'USER', NOW(), NOW()),
-                                                        (2, 'MODERATOR', NOW(), NOW()),
-                                                        (3, 'ADMIN', NOW(), NOW());
+INSERT INTO role (id, name, created_at, updated_at) VALUES (1, 'USER', NOW(), NOW());
+INSERT INTO role (id, name, created_at, updated_at) VALUES (2, 'MODERATOR', NOW(), NOW());
+INSERT INTO role (id, name, created_at, updated_at) VALUES (3, 'ADMIN', NOW(), NOW());
